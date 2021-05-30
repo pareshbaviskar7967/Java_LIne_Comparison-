2c2f8c71146479a7e9ed5b7e5aeff32d97556d3f
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-05-30 13:12:42 +0530

    Line Comparison with OOP's Concept

--- LineComparison.java
+++ LineComparison.java
@@ -1,10 +1,13 @@
 import java.util.*;
 
+import javax.sound.sampled.Line;
+
 class LineComparison {
-    public static void main(String[] args) {
-        double x1, x2, x3, x4, y1, y2, y3, y4;
-        double dist1, dist2;
+
+    public double LineComp() {
         Scanner sc = new Scanner(System.in);
+        double x1, x2, y1, y2;
+
         System.out.println("Enter value for x1: ");
         x1 = sc.nextDouble();
 
@@ -17,29 +20,27 @@ class LineComparison {
         System.out.println("Enter value for y2: ");
         y2 = sc.nextDouble();
 
-        dist1 = (Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
-        System.out.println("Length is " + dist1);
-        String l1 = String.valueOf(dist1);
+        return (Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+    }
 
-        System.out.println("Enter value for x3: ");
-        x3 = sc.nextDouble();
+    public static void main(String[] args) {
 
-        System.out.println("Enter value for y3: ");
-        y3 = sc.nextDouble();
+        LineComparison compare1 = new LineComparison();
 
-        System.out.println("Enter value for x4: ");
-        x4 = sc.nextDouble();
+        System.out.println("Enter the Co-Ordinates 4 1st line : ");
+        double first = compare1.LineComp();
+        System.out.println("Length is " + first);
 
-        System.out.println("Enter value for y4: ");
-        y4 = sc.nextDouble();
+        System.out.println("Enter the Co-Ordinates 4 2nd line : ");
+        double second = compare1.LineComp();
+        System.out.println("Length is " + second);
 
-        dist2 = (Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3)));
-        System.out.println("Length is " + dist2);
-        String l2 = String.valueOf(dist2);
+        String frstobj = String.valueOf(first);
+        String scndobj = String.valueOf(second);
 
-        int length_compare = l1.compareTo(l2);
+        int length_compare = frstobj.compareTo(scndobj);
 
-        if (length_compare>0)
+        if (length_compare > 0)
             System.out.println("Length of line L1 is greater than line L2  ");
         else if (length_compare < 0)
             System.out.println("Length of line L1 is smaller than line L2  ");
